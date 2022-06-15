870c3f4fa38a72f59506aacab3405751b62935a1
oryo-dev <oryo.dev@gmail.com>
2022-06-16 04:26:45 +0900

    Initial commit

---
+++ ScreenTransition/ScreenTransition/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScreenTransition
+//
+//  Created by oryo on 2022/06/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
